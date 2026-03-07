a3d1f1cba00acfef357f8354867869b59c466d9c
hansid <35921016+hansid@users.noreply.github.com>
2026-03-07 12:07:05 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ make info
 # 主配置名称
 PROFILE="generic"
 
-make image PROFILE="$PROFILE" PACKAGES="apk-mbedtls base-files ca-bundle dnsmasq dropbear e2fsprogs firewall4 fstools grub2-bios-setup kmod-button-hotplug kmod-nft-offload libc libgcc libustream-mbedtls logd mkf2fs mtd netifd nftables odhcp6c odhcpd-ipv6only partx-utils ppp ppp-mod-pppoe procd-ujail uci uclient-fetch urandom-seed urngd kmod-amazon-ena kmod-amd-xgbe kmod-bnx2 kmod-dwmac-intel kmod-e1000e kmod-e1000 kmod-forcedeth kmod-fs-vfat kmod-igb kmod-igc kmod-ixgbe kmod-r8169 kmod-tg3 kmod-drm-i915 luci"
+make image PROFILE="$PROFILE" PACKAGES="luci"
