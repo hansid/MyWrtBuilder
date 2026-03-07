d694700a44963dc2157f0f08a1f1639fd469cfeb
hansid <35921016+hansid@users.noreply.github.com>
2026-03-07 21:31:44 +0800

    Create build-imm.sh

---
+++ scripts/build-imm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Start Builder Patch !"
+echo "Current Path: $PWD"
+
+cd $GITHUB_WORKSPACE/$VENDOR-imagebuilder-$VERSION-x86-64.Linux-x86_64 || exit
+
+# Remove redundant default packages
+#sed -i "/luci-app-cpufreq/d" include/target.mk
+
+# Force opkg to overwrite files
+#sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrite/" Makefile
+
+# Not generate ISO images for it is too big
+sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
+
+# Not generate VHDX images
+sed -i "s/CONFIG_VHDX_IMAGES=y/# CONFIG_VHDX_IMAGES is not set/" .config
+
+# Increase rootfs partition size to 500M
+sed -i "s/CONFIG_TARGET_KERNEL_PARTSIZE=32/CONFIG_TARGET_KERNEL_PARTSIZE=512/" .config
+sed -i "s/CONFIG_TARGET_ROOTFS_PARTSIZE=300/CONFIG_TARGET_ROOTFS_PARTSIZE=4096/" .config
+
+sed -i "s/CONFIG_TARGET_PREINIT_IP="192.168.1.1"/CONFIG_TARGET_PREINIT_IP="192.168.31.1"/" .config
+
+sed -i "s/CONFIG_TARGET_PREINIT_BROADCAST="192.168.1.255"/CONFIG_TARGET_PREINIT_BROADCAST="192.168.31.255"" .config
